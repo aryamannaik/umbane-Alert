09622a6a3bb4c57f980c7cc3596df6c31315e6d0
aryamannaik <aryamannaik95@gmail.com>
2015-06-10 17:19:22 +0200

    Pre Calling Functons in View

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,14 +1,15 @@
 class WelcomeController < ApplicationController
-  def index
-  	if current_user
-	  	response = Typhoeus.get "http://whereismypower.co.za/api/get_next_loadshedding?area=#{current_user.areacode}&stage=3B"
-	  	results = JSON.parse(response.body)
-	  	@next_outage = results["next_outage"]
-	  	@next_outage_period = results["next_outage_period"]
+	account_sid = 'ACcc25a6efd339b0a50ad3e05051cc6187'
+	auth_token = '385586a05a54de87e704504dfbff1e11'
+	def index
+		if current_user
+			response = Typhoeus.get "http://whereismypower.co.za/api/get_next_loadshedding?area=#{current_user.areacode}&stage=3B"
+			results = JSON.parse(response.body)
+			@next_outage = results["next_outage"]
+			@next_outage_period = results["next_outage_period"]
 		else
 			@next_outage = []
 			@next_outage_period = []
-
 			1.upto(16).each do |n|
 				response = Typhoeus.get "http://whereismypower.co.za/api/get_next_loadshedding?area=#{n}&stage=3B"
 				results = JSON.parse(response.body)
@@ -16,5 +17,17 @@ class WelcomeController < ApplicationController
 				@next_outage_period << results["next_outage_period"]
 			end
 		end
-  end
+	end
+	helper_method :sendtext
+	def sendtext
+		@client = Twilio::REST::Client.new account_sid, auth_token
+		from = '+15005550006'
+
+		@users.each do |user|
+			client.account.messages.create(
+			:from => from,
+			:to => user.phonenum,
+			:body => "Next Outage at: #{@next_outage[user.areacode]}\nNext Outage duration: #{next_outage_period[user.areacode]}")
+		end
+	end
 end
--- app/controllers/why_controller.rb
+++ app/controllers/why_controller.rb
@@ -1,23 +1,4 @@
 class WhyController < ApplicationController
 	def index
-		@why_information = "What is load shedding?
-Load shedding is the term for when areas lose access to power for a predetermined 
-duration of time. Due to the growing South African economy and increased demand 
-for electricity the Eskom power stations do not have the capacity to meet this 
-demand. Therefore, the load shedding times are planned as a way to throttle the 
-load and share the available electricity among all customers. It is a measure of last 
-resort to prevent the collapse of the power system, which would leave everyone 
-without power for an extended period. 
-
-Why is it important?
-When load shedding occurs, people electrical supply is shut off and they cannot use 
-their Electric geyser microwave ovens, refrigerators, lights, and TV. Since load 
-shedding was instituted in January of 2008, many people have accepted it as a 
-routine part of life and plan around it. There are many mobile apps that notify users 
-when load shedding is going to occur in their area. However, many residents of 
-Townships and low-income areas do not own smartphones. And those that do own 
-them do not know how to utilize them to find out this information. Thus they are 
-taken by surprise when load shedding occurs and do not plan in preparation, that is 
-where we come in."
 	end
 end
