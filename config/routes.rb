4990fcddbb11cb39a6c913aed5a5f09885d9c5cd
aryamannaik <aryamannaik95@gmail.com>
2015-06-10 11:43:01 +0200

    About Us, Why

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
   resources :welcome
+  resources :aboutus
+  resources :why
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
